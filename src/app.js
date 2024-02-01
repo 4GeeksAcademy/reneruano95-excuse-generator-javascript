a5e7dfebf5d36320cfe439165aaa2d9c67b9dad2
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-02-01 15:36:33 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomIndexWho = Math.floor(Math.random() * 4);
+  let randomIndexAction = Math.floor(Math.random() * 4);
+  let randomIndexWhat = Math.floor(Math.random() * 3);
+  let randomIndexWhen = Math.floor(Math.random() * 5);
+
+  const elem = document.getElementById("excuse");
+  elem.innerHTML = `${who[randomIndexWho]} ${action[randomIndexAction]} ${what[randomIndexWhat]} ${when[randomIndexWhen]}`;
 };
